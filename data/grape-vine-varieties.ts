eef5f7bb99f3bfad3857a22b3cf68926ab1b3001
shuabrannigan <joshua.lomas89@gmail.com>
2021-01-04 14:09:06 +1030

    first commit - inital data object which can be used.

---
+++ data/grape-vine-varieties.ts
@@ -0,0 +1,57 @@
+/**
+ * Grape Variety Data - intended to be used as part of a service [angular]
+ *
+ */
+
+export interface VarietyData {
+  id: string;
+  displayName: string;
+  //* any keys the object might need in the future should be added here.
+}
+
+
+/**
+ * inital data values.
+ * The intention is that this data object grows over time. 
+ */
+
+let data: VarietyData[] = [
+  { id: "barbera", displayName: "Barbera" },
+  { id: "cabernetfranc", displayName: "Cabernet Franc" },
+  { id: "cabernetsauvignon", displayName: "Cabernet Sauvignon" },
+  { id: "chardonnay", displayName: "Chardonnay" },
+  { id: "cheninblanc", displayName: "Chenin Blanc" },
+  { id: "carignan", displayName: "Carignan" },
+  { id: "carmenere", displayName: "Carmenere" },
+  { id: "clairette", displayName: "Clairette" },
+  { id: "colombard", displayName: "Colombard" },
+  { id: "durif", displayName: "Durif" },
+  { id: "fiano", displayName: "Fiano" },
+  { id: "grenache", displayName: "Grenache" },
+  { id: "gerwurztraminer", displayName: "Gerwurztraminer" },
+  { id: "malbec", displayName: "Malbec" },
+  { id: "marsanne", displayName: "Marsanne" },
+  { id: "mataromourvedre", displayName: "Mataro/Mourvedre" },
+  { id: "merlot", displayName: "Merlot" },
+  { id: "montepulciano", displayName: "Montepulciano" },
+  { id: "muscat ", displayName: "Muscat " },
+  { id: "nerodavola", displayName: "Nero d'Avola" },
+  { id: "petitverdot", displayName: "Petit Verdot" },
+  { id: "pinotgrisgrigio", displayName: "Pinot Gris (Grigio)" },
+  { id: "pinotnoir", displayName: "Pinot Noir" },
+  { id: "riesling", displayName: "Riesling" },
+  { id: "roussanne", displayName: "Roussanne" },
+  { id: "rubycabernet", displayName: "Ruby Cabernet" },
+  { id: "sangiovese", displayName: "Sangiovese" },
+  { id: "sauvignonblanc", displayName: "Sauvignon Blanc" },
+  { id: "semillon", displayName: "Semillon" },
+  { id: "shiraz", displayName: "Shiraz" },
+  { id: "tempranillo", displayName: "Tempranillo" },
+  { id: "touriganacional", displayName: "Touriga Nacional" },
+  { id: "traminer", displayName: "Traminer" },
+  { id: "verdelho", displayName: "Verdelho" },
+  { id: "vermentino", displayName: "Vermentino" },
+  { id: "viognier", displayName: "Viognier" },
+  { id: "zinfandel", displayName: "Zinfandel" },
+  { id: "other", displayName: "Other " },
+];
